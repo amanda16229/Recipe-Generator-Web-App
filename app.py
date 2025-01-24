648d7ad78b106b748234236bc3e513771e0633a5
Amanda <amanda.olyer@gmail.com>
2025-01-24 00:59:10 +0000

    added files via upload
    The main Python code is updated to handle different pages of the web app and contains POST and GET requests. Note: The API key in the code is not my actual key.

--- app.py
+++ app.py
@@ -1,21 +1,69 @@
-from flask import Flask
-import requests # makes a call and it is checked with the help of status code that whether our request was successful or not
+from urllib.parse import unquote
+from flask import Flask, render_template, request
+# import requests  makes a call & its checked w/ help of status code that whether our request was successful or not
+
+# API_URL = 'https://api.spoonacular.com'
 
 # Flask constructor takes the name of current module (__name__) as argument.
 app = Flask(__name__) # main app instance
 
-API_URL = 'https://api.spoonacular.com/recipes/complexSearch'
-API_KEY = '3b85999be9b34d5aa21904bef8cf4749'
+API_KEY = '123'
 
-# Making the API request
-response = requests.get(API_URL)
+@app.route('/', methods=["POST", "GET"]) 
+def search():
+    # since we're searching for recipes, POST and GET requests need to be handled
+    # if user submits a recipe search, activate POST method
+    if request.method == "POST": # server request
+        query = request.form.get('search_query', '')
+        recipes = search_recipes(query)
+        return render_template("index.html", recipes=recipes, search_query = query)
+    
+    else: # if no search submitted, still need to handle this possibility, rendering page w.out searches
+        search_query = request.args.get('search_query', '')
+        decoded_search_query = unquote(search_query)
+        recipes = search_recipes(decoded_search_query)
+        return render_template("index.html", recipes)
 
-# what info / data am i accessing and how to translate to usable code?
-@app.route('/') # accessing root of app
-def home():
-    return "Welcome to this Recipe Generator!"
 
+def search_recipes(query):
+    url = f'https://api.spoonacular.com/recipes/complexSearch'
+    params = {
+        'apiKey': API_KEY,
+        'query': query,
+        'number': 10,
+        'instructionsRequired': True,
+        'addRecipeInformation': True,
+        'fillIngredients': True,
+    }
+
+    # Send a GET request to the Spoonacular API with the query parameters
+    response = request.get(url, params=params)
+    if response.status_code == 200: # server response
+        data = response.json()
+        return data['results']
+    else:
+        return []
+
+# displays list of recipes on this page
+@app.route('/recipe/<int:recipe_id>')
+def view_recipe(recipe_id):
+    search_query = request.args.get('search_query', '')
+    render_template("view_recipe.html")
+
+    # API request for single recipe's info
+    url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+    params = {
+        'apiKey': API_KEY
+    }
+
+    response = request.get(url, params=params)
+    if response.status_code == 200: # server response
+        recipe = response.json()
+        # takes usr to page for single recipe info
+        return render_template("view_recipe.html", recipe=recipe, search_query=search_query)
+    else:
+        return "Recipe not found", 404
 
 # main driver function
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
